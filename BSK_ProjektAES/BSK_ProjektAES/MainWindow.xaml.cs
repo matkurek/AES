baa881fecaa332e5def1f3cc652eb4f5ab0b7e38
matkurek <43915566+matkurek@users.noreply.github.com>
2019-05-06 19:52:00 +0200

    Add files via upload

---
+++ BSK_ProjektAES/BSK_ProjektAES/MainWindow.xaml.cs
@@ -0,0 +1,253 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace BSK_ProjektAES
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            listBox.Items.Add("CBC");
+            listBox.Items.Add("CFB");
+            listBox.Items.Add("CTS");
+            listBox.Items.Add("ECB");
+            listBox.Items.Add("OFB");
+        }
+
+
+        //wybieranie pliku
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op= new OpenFileDialog();
+            op.DefaultExt = ".txt";
+
+            bool? result = op.ShowDialog();
+            if (result.HasValue)
+            {
+                FileNameText.Text = op.FileName;
+                long length = new System.IO.FileInfo(FileNameText.Text).Length;
+                length /= 1000;
+                infoBox.Text += "Wybrano plik " + op.FileName + " (" + length.ToString() + " KB)\n";
+            }
+
+        }
+
+        //szyfrowanie
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string password = "ThePasswordToDecryptAndEncryptTheFile";
+
+            // For additional security Pin the password of your files
+            GCHandle gch = GCHandle.Alloc(password, GCHandleType.Pinned);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            infoBox.Text += "Szyfuję wybrany plik..." + "\n";
+            // Encrypt the file
+            FileEncrypt(FileNameText.Text, password);
+
+            stopwatch.Stop();
+            long elapsed_time = stopwatch.ElapsedMilliseconds;
+
+            // To increase the security of the encryption, delete the given password from the memory !
+            ZeroMemory(gch.AddrOfPinnedObject(), password.Length * 2);
+            gch.Free();
+
+            // You can verify it by displaying its value later on the console (the password won't appear)
+            Console.WriteLine("The given password is surely nothing: " + password);
+
+            infoBox.Text += "Zaszyfrowano plik metodą szyfrowania " + listBox.SelectedItem.ToString() + "\n";
+            infoBox.Text += "Czas szyfrowania: " + elapsed_time.ToString() + " ms\n";
+
+            long length = new System.IO.FileInfo(FileNameText.Text).Length;
+            float szybkoscKB = length / elapsed_time; //milisekundy skracają się z bajtami dając kilobajty na sekunde
+
+
+            infoBox.Text += "Szybkość szyfrowania: " + szybkoscKB.ToString() + " KB/s\n";
+        }
+
+        //  Call this function to remove the key from memory after use for security
+        [DllImport("KERNEL32.DLL", EntryPoint = "RtlZeroMemory")]
+        public static extern bool ZeroMemory(IntPtr Destination, int Length);
+
+        /// <summary>
+        /// Creates a random salt that will be used to encrypt your file. This method is required on FileEncrypt.
+        /// </summary>
+        /// <returns></returns>
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    // Fille the buffer with the generated data
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Encrypts a file from its path and a plain password.
+        /// </summary>
+        /// <param name="inputFile"></param>
+        /// <param name="password"></param>
+        private void FileEncrypt(string inputFile, string password)
+        {
+            //http://stackoverflow.com/questions/27645527/aes-encryption-on-large-files
+
+            //generate random salt
+            byte[] salt = GenerateRandomSalt();
+
+            //create output file name
+            FileStream fsCrypt = new FileStream(inputFile + ".aes", FileMode.Create);
+
+            //convert password string to byte arrray
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            //Set Rijndael symmetric encryption algorithm
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            //http://stackoverflow.com/questions/2659214/why-do-i-need-to-use-the-rfc2898derivebytes-class-in-net-instead-of-directly
+            //"What it does is repeatedly hash the user password along with the salt." High iteration counts.
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            //Cipher modes: http://security.stackexchange.com/questions/52665/which-is-the-best-cipher-mode-and-padding-mode-for-aes-encryption
+            AES.Mode = CipherMode.CFB;
+
+            // write salt to the begining of the output file, so in this case can be random every time
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    //Application.DoEvents(); // -> for responsive GUI, using Task will be better!
+                    cs.Write(buffer, 0, read);
+                }
+
+                // Close up
+                fsIn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                cs.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        /// <summary>
+        /// Decrypts an encrypted file with the FileEncrypt method through its path and the plain password.
+        /// </summary>
+        /// <param name="inputFile"></param>
+        /// <param name="outputFile"></param>
+        /// <param name="password"></param>
+        private void FileDecrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CFB;
+
+            //Wybór trybu szyfrowania
+            string itemText = listBox.SelectedItem.ToString();
+            if (itemText == "CFB") AES.Mode = CipherMode.CFB;
+            if (itemText == "CFB") AES.Mode = CipherMode.CFB;
+            if (itemText == "CTS") AES.Mode = CipherMode.CTS;
+            if (itemText == "ECB") AES.Mode = CipherMode.ECB;
+            if (itemText == "OFB") AES.Mode = CipherMode.OFB;
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    //Application.DoEvents();
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+
+    }
+}
